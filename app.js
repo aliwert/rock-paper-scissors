3ddb4c0500e63cc4bc5b68d4d3d65711a0c4bf9c
aliwert <154356044+aliwert@users.noreply.github.com>
2024-03-21 21:39:41 +0300

    jsupdated

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-// set up the core function for the computer that will use math.random to loop through an array and return that value
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomNumber = Math.floor(Math.random() * 3);
@@ -66,20 +65,19 @@ function draw(user, computer) {
 // The core game functions that set up and determine the games actual logic aka paper beats rock etc
 function game(userChoice) {
   const computerChoice = getComputerChoice();
-  // console.log('Game function: user choice is = ' + userChoice);
-  // console.log('Game function: computer choice is = ' + computerChoice);
+
   switch (userChoice + computerChoice) {
     case "paperrock":
     case "rockscissors":
     case "scissorspaper":
       win(userChoice, computerChoice);
-      // console.log("user wins");
+      
       break;
     case "rockpaper":
     case "scissorsrock":
     case "paperscissors":
       loses(userChoice, computerChoice);
-      // console.log("computer wins");
+      
       break;
     case "rockrock":
     case "scissorsscissors":
@@ -89,20 +87,7 @@ function game(userChoice) {
       break;
   }
 }
-// ES5 style of writing this function
-// function main() {
-// rock_div.addEventListener('click', function() {
-// game('rock');
-// });
-// paper_div.addEventListener('click', function() {
-// game('paper');
-// });
-// scissors_div.addEventListener('click', function() {
-// game('scissors');
-// });
-// }
-// ES6 style of writing this function
-// This function creates and adds an eventlistener to the rock, paper scissors html element and the passes the value of that element to the game function
+
 
 function main() {
   rock_div.addEventListener("click", () => game("rock"));
